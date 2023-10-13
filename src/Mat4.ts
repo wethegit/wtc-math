87580a912bd78726242591611b717cfb9b5a8111
Liam Egan <liam@wethecollective.com>
2023-10-13 16:24:18 -0700

    Adding iteration to the objects

--- src/Mat4.ts
+++ src/Mat4.ts
@@ -1474,6 +1474,25 @@ class Mat4 {
       );
     }
   }
+  
+  /**
+   * Adds iteration to the object, allowing it 
+   * to be destructured and iterated upon in 
+   * various useful ways.
+  */
+  [Symbol.iterator]() {
+    let values = this.array;
+    let index = 0;
+    return {
+      next() {
+        if (index < values.length) {
+          let value = values[index];
+          index++;
+          return { value, done: false };
+        } else return { done: true }
+      }
+    }
+  }
 }
 
 export { Mat4 };
