4f8e8fbab77a625889852379d53a6730765399d4
Marlon Ugocioni Marcello <marlon.marcello@gmail.com>
2024-05-17 09:17:15 -0700

    hotfix: updates to node 20 (#47)

---
+++ documentation/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4uuVipJrShRslLyTSwxUtJRKkgsyQDyknMSi4tTi/VBonoZJbk5QKnszLwUJStDI4taHWRdxlh1GRPQZYJVlwk+XYGliSWYukCi+HSFpSZj8RdIlIAuLP4CiRLQhcVfIFFMXbEAPYrSonsBAAA="
